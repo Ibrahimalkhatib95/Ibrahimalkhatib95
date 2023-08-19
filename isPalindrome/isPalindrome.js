f30d9689cca4d2dbd6b36a9ba8f8d47a4d1582cd
Ibrahimalkhatib95 <142555445+Ibrahimalkhatib95@users.noreply.github.com>
2023-08-19 15:47:28 +0300

    Palindrome
    tasked with implementing a function that determines whether a given string is a valid palindrome or not. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward, disregarding spaces, punctuation, and capitalization.
    
    Write a JavaScript function called isPalindrome that takes a string as input and returns true if the input string is a valid palindrome, and false otherwise.

---
+++ isPalindrome/isPalindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
+   console.log(isPalindrome('A man, a plan, a canal, Panama!'));
+   console.log(isPalindrome('Race car')); 
+   console.log(isPalindrome('Hello, world!')); 
